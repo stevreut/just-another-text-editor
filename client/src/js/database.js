4185454c5c31cfeb0bd984c6fac4b856a221170d
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-22 20:28:38 -0400

    getDb and putDb implemented but having key issues on put

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('beginning idb put');
+  try {
+    const jateDb = await openDB('jate', 1);  // TODO - what does the 1 do?
+    const tx = jateDb.transaction('jate', 'readwrite');  // TODO - This jate is DB?
+    const store = tx.objectStore('jate');  // TODO - This jate is store?  (maybe make them different later?)
+    const request = store.put({ content: content }, 1 /*key*/);  // TODO - we just always use 1
+    const result = await request;
+    console.log('data saved = "', request, '"');
+    console.log('ending idb put');
+  } catch (err) {
+    console.log('err on db put = ', err.message);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async() => {
+  console.log('beginning db get');
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);  // TODO - same key as used in put
+    const result = await request;
+    console.log('db get result = ', result);
+    console.log('end db get');
+    return result;
+  } catch (err) {
+    console.log('err on db get = "', err.message, '"');
+  }
+}
 
 initdb();
