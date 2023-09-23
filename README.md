e1ab6d721669b4840541c2bab78c795a5cb97ad3
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-22 21:33:49 -0400

    db put now semi-successful (displaying promised rather than content

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ TODO - TBD
 REMINDER:  
 Again make sure in your client folder you do the following:
   npm i @babel/plugin-proposal-object-rest-spread
+
+
+
+Be sure to attribute:
+  Class materials
+  https://www.npmjs.com/package/idb
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -8,6 +8,7 @@ const initdb = async () =>
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      // db.createObjectStore('jate');  // TODO - see if this solves are put error problem
       console.log('jate database created');
     },
   });
@@ -15,12 +16,12 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
 export const putDb = async (content) => {
-  console.log('beginning idb put');
+  console.log('beginning idb put version 9:30');  // TODO
   try {
     const jateDb = await openDB('jate', 1);  // TODO - what does the 1 do?
     const tx = jateDb.transaction('jate', 'readwrite');  // TODO - This jate is DB?
     const store = tx.objectStore('jate');  // TODO - This jate is store?  (maybe make them different later?)
-    const request = store.put({ content: content }, 1 /*key*/);  // TODO - we just always use 1
+    const request = store.put({ id: 1, content: content });  // TODO - we just always use 1
     const result = await request;
     console.log('data saved = "', request, '"');
     console.log('ending idb put');
@@ -32,16 +33,16 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async() => {
-  console.log('beginning db get');
+  console.log('beginning db get version 9:30');  // TODO
   try {
     const jateDb = await openDB('jate', 1);
     const tx = jateDb.transaction('jate', 'readonly');
     const store = tx.objectStore('jate');
     const request = store.get(1);  // TODO - same key as used in put
     const result = await request;
-    console.log('db get result = ', result);
+    console.log('db get result = ', JSON.stringify(result));;
     console.log('end db get');
-    return result;
+    return result.content;
   } catch (err) {
     console.log('err on db get = "', err.message, '"');
   }
